2a91c37106a1dbe4e1fbab6fa5447f4763503167
Dalkeshwar <120374152+Dalkeshwar@users.noreply.github.com>
2025-01-23 00:08:23 +0530

    Update s3.tf

--- code/build/s3.tf
+++ code/build/s3.tf
@@ -6,16 +6,7 @@ resource "aws_s3_bucket" "dev_s3" {
   bucket_prefix = "dev-"
 
   tags = {
-    Environment          = "Dev"
-    git_commit           = "9bdafcaf0c264dfc7d88c069e75cde1f52bf18ae"
-    git_file             = "code/build/s3.tf"
-    git_last_modified_at = "2025-01-22 18:19:26"
-    git_last_modified_by = "120374152+Dalkeshwar@users.noreply.github.com"
-    git_modifiers        = "120374152+Dalkeshwar"
-    git_org              = "Dalkeshwar"
-    git_repo             = "prisma-cloud-devsecops-workshop"
-    yor_name             = "dev_s3"
-    yor_trace            = "35fa4f6a-c881-4270-a7b0-72b16bd6717d"
+    Environment      = "Dev"
   }
 }
 
